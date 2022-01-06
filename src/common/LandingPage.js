b0d27d50e4cd3d558d6f6289ce38e94183fd686d
Sally Sghair <sallysghair1@gmail.com>
2022-01-06 23:20:01 +0000

    morecss

--- src/common/LandingPage.js
+++ src/common/LandingPage.js
@@ -1,23 +1,20 @@
 import React from 'react'
+import { Carousel } from '3d-react-carousal'
 
 
 function LandingPage(){
+  const slides = [
+    <img  src="../assets/breakfast.jpeg" alt="1" height="600px" width="700px" />,
+    <img  src="../assets/lunch.jpeg" alt="2" height="600px" width="700px"/>,
+    <img src="../assets/dinner.jpeg" alt="3" height="600px" width="700px"/>   ]
 
   return (
-    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src="../assets/breakfast.jpeg" className="d-block w-100" alt="..."/>
-        </div>
-        <div className="carousel-item">
-          <img src="../assets/lunch.jpeg" className="d-block w-100" alt="..."/>
-        </div>
-        <div className="carousel-item">
-          <img src="../assets/dinner.jpeg" className="d-block w-100" alt="..."/>
-        </div>
-      </div>
-    </div>
+    <><Carousel slides={slides} autoplay={false} interval={3000} /><div>
+      {/* <h1>KetoKitchen</h1> */}
+    </div></>
+    
+
   )
 }
-
+  
 export default LandingPage
