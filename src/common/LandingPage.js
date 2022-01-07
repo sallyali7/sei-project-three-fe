5299687041517ea468f91756277e0c6fa6f2d062
Aromi Afolabi <aromiafolabi@googlemail.com>
2022-01-07 14:18:38 +0000

    fixed key prompt on landingpage

--- src/common/LandingPage.js
+++ src/common/LandingPage.js
@@ -4,9 +4,9 @@ import { Carousel } from '3d-react-carousal'
 
 function LandingPage(){
   const slides = [
-    <img  src="../assets/breakfast.jpeg" alt="1" height="600px" width="700px" />,
-    <img  src="../assets/lunch.jpeg" alt="2" height="600px" width="700px"/>,
-    <img src="../assets/dinner.jpeg" alt="3" height="600px" width="700px"/>   ]
+    <img  src="../assets/breakfast.jpeg" alt="1" height="600px" width="700px" key="1"/>,
+    <img  src="../assets/lunch.jpeg" alt="2" height="600px" width="700px" key="2"/>,
+    <img src="../assets/dinner.jpeg" alt="3" height="600px" width="700px" key="3"/>   ]
 
   return (
     <><Carousel slides={slides} autoplay={false} interval={3000} /><div>
